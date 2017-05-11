c08ac7da66a569060945af7891c1bc81f64c4e55
walter_wu090 <walter.wu090@gmail.com>
2017-05-11 02:44:06 -0400

    moved all custom transformers under one file, removed pipeline_ready.py

--- transformers.py
+++ transformers.py
@@ -7,6 +7,7 @@ class StringOneHotEncoder(TransformerMixin, BaseEstimator):
     one hot encoder for string values
     inheritor of TransformerMixin, can be used in a sklearn pipeline
     """
+
     def __init__(self):
         """
         constructor
@@ -29,3 +30,25 @@ class StringOneHotEncoder(TransformerMixin, BaseEstimator):
 
     def inverse_transform(self, x):
         raise NotImplementedError
+
+
+class FeatureExtractor(BaseEstimator, TransformerMixin):
+    """
+    extracts feature data for sklearn feature union
+    """
+
+    def __init__(self, features):
+        """
+        constructor
+        :param features: string value, names of the feature to extract
+        """
+        self.features = features
+
+    def fit(self):
+        return self
+
+    def transform(self, x):
+        return x[self.features]
+
+    def fit_transform(self, x, y=None, **fit_params):
+        return self.transform(x)
